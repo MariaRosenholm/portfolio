0fdb65f3bec49694b955a9006f824a073afe76ae
Miss Maria <contact@mariarosenholm.com>
2023-12-18 19:56:14 +0200

    backgroud static text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Maria</h1>
+      <h2>Rosenholm</h2>
+      <h3>Web</h3>
+      <h4>developer</h4>
+    </div>
+  );
 }
 
 export default App;
